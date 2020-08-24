b91895dd8c12ab837a62e0bbd19d62e8d0889311
Darwin Pérez Albores <perezalboresd@gmail.com>
2020-08-24 17:31:28 -0500

    version 1

---
+++ script.js
@@ -0,0 +1,10 @@
+function open_card() {
+  document.getElementById('cerrar').style.display = '';
+  document.getElementById('outside').className = 'open-card';
+  
+}
+
+function close_card() {
+  document.getElementById('outside').className = '';
+  document.getElementById('cerrar').style.display = "none";
+}
